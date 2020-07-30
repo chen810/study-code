da0ec5a4935e19096babd2d492183ec065bd30a1
chen810 <15150765131@163.com>
2020-07-30 13:07:33 +0800

    UTF-8的MD5亿次加密

--- python38/test.py
+++ python38/test.py
@@ -1,2 +1,14 @@
-for i in range(100):
-    print("hello")
+import hashlib
+msg = "77.82%"
+'''
+for i in range(100000000):
+    if i % 1000000 == 0:
+        print("{0}==>{1}".format(i, msg))
+    p = hashlib.md5()
+    p.update(msg.encode('utf-8'))
+    msg = p.hexdigest()
+'''
+p = hashlib.md5()
+p.update(msg.encode('utf-8'))
+msg = p.hexdigest()
+print(msg)
