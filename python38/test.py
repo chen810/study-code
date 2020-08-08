d3c308387389d8c06610ff2087632134069b0cc6
chen810 <15150765131@163.com>
2020-08-08 20:10:52 +0800

    t

--- python38/test.py
+++ python38/test.py
@@ -1,14 +1,11 @@
 import hashlib
-msg = "77.82%"
-'''
+msg = "73.25%"
+
 for i in range(100000000):
     if i % 1000000 == 0:
         print("{0}==>{1}".format(i, msg))
     p = hashlib.md5()
-    p.update(msg.encode('utf-8'))
+    p.update(msg.encode(encoding= 'utf-8'))
     msg = p.hexdigest()
-'''
-p = hashlib.md5()
-p.update(msg.encode('utf-8'))
-msg = p.hexdigest()
+
 print(msg)
