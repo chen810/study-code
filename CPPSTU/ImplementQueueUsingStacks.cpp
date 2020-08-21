33391202fe66031a02b5a68ea610285d36e7adef
chen810 <15150765131@163.com>
2020-08-21 23:53:19 +0800

    Implement Queue using Stacks

---
+++ CPPSTU/ImplementQueueUsingStacks.cpp
@@ -0,0 +1,45 @@
+// 用栈实现队列
+/**
+ * 使用栈实现队列的下列操作：
+ * push(x) -- 将一个元素放入队列的尾部
+ * pop() -- 从队列首部移除元素
+ * peek() -- 返回队列首部的元素
+ * empty() -- 返回队列是否为空
+ * */
+
+class MyQueue {
+public:
+    /** Initialize your data structure here. */
+    MyQueue() {
+
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+
+    }
+    
+    /** Get the front element. */
+    int peek() {
+
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
