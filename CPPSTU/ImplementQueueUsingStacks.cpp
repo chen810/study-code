bcdcd9e24d8d69d791aeaf5a5caf625691487e40
chen810 <15150765131@163.com>
2020-08-22 00:08:56 +0800

    Implement Queue using Stacks

--- CPPSTU/ImplementQueueUsingStacks.cpp
+++ CPPSTU/ImplementQueueUsingStacks.cpp
@@ -7,31 +7,54 @@
  * empty() -- 返回队列是否为空
  * */
 
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
 class MyQueue {
+    stack<int> *dataIn;
+    stack<int> *dataOut;
+    int capacity;
 public:
     /** Initialize your data structure here. */
     MyQueue() {
-
+        dataIn = new stack<int>;
+        dataOut = new stack<int>;
     }
     
     /** Push element x to the back of queue. */
     void push(int x) {
-
+        dataIn->push(x);
     }
     
     /** Removes the element from in front of queue and returns that element. */
     int pop() {
-
+        if(dataOut->empty()){
+            while(!dataIn->empty()){
+                dataOut->push(dataIn->top());
+                dataIn->pop();
+            }
+        }
+        int res = dataOut->top();
+        dataOut->pop();
+        return res;
     }
     
     /** Get the front element. */
     int peek() {
-
+        if(dataOut->empty()){
+            while(!dataIn->empty()){
+                dataOut->push(dataIn->top());
+                dataIn->pop();
+            }
+        }
+        return dataOut->top();
     }
     
     /** Returns whether the queue is empty. */
     bool empty() {
-
+        return dataIn->empty()&&dataOut->empty();
     }
 };
 
