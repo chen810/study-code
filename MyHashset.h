bcf89d3eaac1801e073c1df03e5fa81ffa4e5917
chen810 <15150765131@163.com>
2020-09-29 20:45:06 +0800

    My HashSet AND HashMap

--- MyHashset.h
+++ MyHashset.h
@@ -6,26 +6,9 @@
 #define STUDY_CODE_MYHASHSET_H
 #include <vector>
 #include <iostream>
+#include "MyTree.h"
 using namespace std;
-// 二叉树节点
-struct TreeNode{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int v,TreeNode* l,TreeNode* r):val(v),left(l),right(r){};
-    explicit TreeNode(int v):TreeNode(v, nullptr, nullptr) {};
-};
 
-
-// 向搜索二叉树中加入节点
-TreeNode* addToSearchTree(TreeNode* root, int val);
-// 向搜索二叉树中加入搜索二叉树
-TreeNode* addToSearchTreeByTree(TreeNode* root, TreeNode* subNode);
-// 从搜索二叉树中移除节点
-TreeNode* removeFromSearchTree(TreeNode* root, int val);
-// 在搜索二叉树中搜索节点
-bool checkInSearchTree(TreeNode* root, int val);
-// 哈希表的搜索二叉树实现
 class MyHashSet {
     vector<TreeNode *> *data;
 public:
